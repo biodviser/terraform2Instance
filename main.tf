88de5bc33542c13f23b7e9bd11f0a626aa563aac
Yogender <yogender027mae@gmail.com>
2022-05-23 16:10:59 +0200

    NACl with ingress and outgress rules+ docker installed

--- main.tf
+++ main.tf
@@ -78,6 +78,15 @@ resource "aws_default_network_acl" "default" {
     to_port    = 0
   }
 
+   ingress {
+    protocol   = -1
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
   egress {
     protocol   = -1
     rule_no    = 100
